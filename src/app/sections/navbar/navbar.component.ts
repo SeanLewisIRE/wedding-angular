3bd93d6f337e064ecdb4ed36944a5e2f6d832f2c
Sean <slewis100@gmail.com>
2023-01-01 19:53:24 +0000

    wip

--- src/app/sections/navbar/navbar.component.ts
+++ src/app/sections/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-
 export class NavbarComponent {
+  menuStyle: string = 'initial-opacity';
   showMenu = false;
   toggleNavbar() {
     this.showMenu = !this.showMenu;
+    this.showMenu ? this.menuStyle = 'full-opacity' : this.menuStyle = 'initial-opacity';
   }
 }
