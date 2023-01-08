cee19a0bcc3d145f957de2811e500e9a815591e9
Sean <slewis100@gmail.com>
2023-01-08 18:38:53 +0000

    multiple images supported

--- src/app/sections/carousel/carousel.component.ts
+++ src/app/sections/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
+  @Input() locationValue: string = '';
+  imageLocation: string[] = [];
+  
 
-  constructor() { }
-
+  constructor() { 
+  }
+  
   ngOnInit(): void {
+    if (this.locationValue == 'first') {
+      this.imageLocation = [
+        'https://picsum.photos/480/570',
+        'https://picsum.photos/480/570',
+        'https://picsum.photos/480/570',
+      ];
+    } else if (this.locationValue == 'second') {
+      this.imageLocation = [
+        'https://picsum.photos/480/560',
+        'https://picsum.photos/480/560',
+        'https://picsum.photos/480/560',
+      ];
+    }
   }
 
 }
