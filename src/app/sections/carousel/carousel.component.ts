81ff0157d2eb8ae23458e2189be4e1733adc4256
Sean <slewis100@gmail.com>
2023-01-09 22:41:09 +0000

    images

--- src/app/sections/carousel/carousel.component.ts
+++ src/app/sections/carousel/carousel.component.ts
@@ -17,14 +17,14 @@ export class CarouselComponent implements OnInit {
     if (this.locationValue == 'first') {
       this.imageLocation = [
         'https://picsum.photos/480/570',
-        'https://picsum.photos/480/570',
+        '../../../assets/venue_side.jpeg',
         'https://picsum.photos/480/570',
       ];
     } else if (this.locationValue == 'second') {
       this.imageLocation = [
+        '../../../assets/stairs.jpeg',
         'https://picsum.photos/480/560',
-        'https://picsum.photos/480/560',
-        'https://picsum.photos/480/560',
+        '../../../assets/pool.jpeg',
       ];
     }
   }
